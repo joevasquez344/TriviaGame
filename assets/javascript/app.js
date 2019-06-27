033580acc60a8c8d2b5d889de62bc94584f6fa14
Joseph Vasquez <josephvasquez@Josephs-MacBook-Pro.local>
2019-06-26 23:13:28 -0700

    last update?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,56 +11,59 @@ var false002 = $('#falseBtn2');
 var true003 = $('#trueBtn3');
 var false003 = $('#falseBtn3');
 
-
-
+$('.start-link').on('click', function(){
+    $('.main-start-container').hide();
+    $('.main-container').show();
+    console.log('ok')
     setInterval(function () {
         timer--;
         $('#timeRemaining').html(timer);
         gameOverFunc();
-        
-    }, 1000)
     
+    }, 1000)
+})
+
+
+
+// setInterval(function () {
+//     timer--;
+//     $('#timeRemaining').html(timer);
+//     gameOverFunc();
+
+// }, 1000)
 
 
 
 
-var gameOverFunc = function(){
+
+var gameOverFunc = function () {
     if (timer <= 0) {
         timer = '';
         $('#timeRemaining').text(timer);
         $('#correctAnswers').text(correctCounter);
-        console.log(correctCounter)
-        ('.main-container').addClass('hide');
+        $('#incorrectAnswers').text(incorrectCounter);
         
+        $('.main-container').hide(100);
+        $('.main-result-container').show(100);
+
+        unanwseredFunc();
+
     }
 }
 
-var correctCounterFunc = function(true001Click){
-    if(true001Click){
-        console.log("yes")
-    }
-}
-correctCounterFunc();
-// var questionArr001 = [];
 
-// var clickTracker = function(e){
-//     // var clicks;
-//     // if(clicks = e.target){
-//     //     questionArr001.push(clicks);
-//     //     alert(questionArr001);
-//     // }
-// }
 
 
 //-------------1st row
 
-true001.on('click', function () {
+true001.on('click', function (e) {
     correctCounter++
     
     true001.addClass('green')
-    
+
     true001.off();
     false001.off();
+
 })
 
 
@@ -69,7 +72,7 @@ false001.on('click', function () {
 
     false001.addClass('red');
     true001.addClass('green');
-    
+
     false001.off();
     true001.off();
 
@@ -79,11 +82,11 @@ false001.on('click', function () {
 
 true002.on('click', function () {
     incorrectCounter++
-    
+
     true002.addClass('red');
     false002.addClass('green');
 
-    
+
     true002.off();
     false002.off();
 })
@@ -95,8 +98,8 @@ false002.on('click', function () {
     correctCounter++;
 
     false002.addClass('green');
-    
-    
+
+
     false002.off();
     true002.off();
 
@@ -106,12 +109,38 @@ false002.on('click', function () {
 
 true003.on('click', function () {
     correctCounter++
-    
+
     true003.addClass('green');
-    
+
     true003.off();
     false003.off();
 })
+
+var unanwseredFunc = function(){
+    // 1 unanswered
+    if(correctCounter === 2 && incorrectCounter === 0){
+        $('#unansweredQuestions').text('1')
+    }else if(correctCounter === 0 && incorrectCounter === 2){
+        $('#unansweredQuestions').text('1')
+    }else if(correctCounter === 1 && incorrectCounter === 1){
+        $('#unansweredQuestions').text('1')
+    }
+    // 2 unanswered
+    else if(correctCounter === 1 && incorrectCounter === 0){
+        $('#unansweredQuestions').text('2')
+    }else if(correctCounter === 0 && incorrectCounter === 1){
+        $('#unansweredQuestions').text('2')
+    }
+    // 3 unanswered
+    else if(correctCounter === 0 && incorrectCounter === 0){
+        $('#unansweredQuestions').text('3')
+    }
+    // 0 unanswered
+    else{
+        $('#unansweredQuestions').text('0')
+    }
+    
+}
 
 
 
@@ -121,17 +150,18 @@ false003.on('click', function () {
 
     false003.addClass('red');
     true003.addClass('green');
-    
+
     false003.off();
     true003.off();
 
 })
 
-$('#doneLink').on('click', function(){
+$('#doneLink').on('click', function () {
     $('.main-container').hide(100);
     $('.main-result-container').show(100);
     $('#correctAnswers').text(correctCounter);
     $('#incorrectAnswers').text(incorrectCounter);
+    unanwseredFunc();
 
 })
 
